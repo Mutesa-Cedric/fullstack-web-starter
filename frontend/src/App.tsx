7dd78647a3fcc5930176c2a11b5e44f7607836fc
Mutesa-Cedric <mutesacedric@gmail.com>
2024-06-10 09:26:54 +0200

    init : frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center text-2xl font-semibold text-gray-600">
+      Hello world
+    </div>
+  )
+}
+
+export default App
