a072bdcf3f3c2338de9cb09dca61f9c899094324
Mutesa-Cedric <mutesacedric@gmail.com>
2024-06-10 21:06:46 +0200

    feat : dashboard layout

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,6 +5,8 @@ import { Link, useNavigate } from "react-router-dom";
 import AuthInput from "../components/AuthInput";
 import AuthLayout from "../components/layouts/AuthLayout";
 import Logo from "../components/Logo";
+// @ts-expect-error(no type definitions found )
+import { Helmet } from "react-helmet";
 
 export default function Login() {
     const [loggingIn, setLoggingIn] = useState(false);
@@ -25,55 +27,60 @@ export default function Login() {
     }
 
     return (
-        <AuthLayout>
-            <div className="mx-auto w-full max-w-sm lg:w-96">
-                <div>
-                    <Link to={'/'}>
-                        <Logo />
-                    </Link>
-                    <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
-                    </h2>
-                    <p className="mt-2 text-sm leading-6 text-gray-500">
-                        Not a member?{' '}
-                        <Link to="/register" className="font-semibold text-blue-600 hover:text-blue-500">
-                            Create an account
-                        </Link>
-                    </p>
-                </div>
-
-                <div className="mt-10">
+        <>
+            <Helmet>
+                <title>Login</title>
+            </Helmet>
+            <AuthLayout>
+                <div className="mx-auto w-full max-w-sm lg:w-96">
                     <div>
-                        <form
-                            onSubmit={handleSubmit}
-                            className="space-y-6">
-                            <AuthInput
-                                label="Email Address"
-                                required
-                                autoComplete="email"
-                                type="email"
-                            />
+                        <Link to={'/'}>
+                            <Logo />
+                        </Link>
+                        <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                            Sign in to your account
+                        </h2>
+                        <p className="mt-2 text-sm leading-6 text-gray-500">
+                            Not a member?{' '}
+                            <Link to="/register" className="font-semibold text-blue-600 hover:text-blue-500">
+                                Create an account
+                            </Link>
+                        </p>
+                    </div>
 
-                            <AuthInput
-                                label="Password"
-                                required
-                                autoComplete="current-password"
-                                type="password"
-                            />
+                    <div className="mt-10">
+                        <div>
+                            <form
+                                onSubmit={handleSubmit}
+                                className="space-y-6">
+                                <AuthInput
+                                    label="Email Address"
+                                    required
+                                    autoComplete="email"
+                                    type="email"
+                                />
 
-                            <div>
-                                <Button
-                                    type="submit"
-                                    className="w-full"
-                                    loading={loggingIn}
-                                >
-                                    Sign in
-                                </Button>
-                            </div>
-                        </form>
+                                <AuthInput
+                                    label="Password"
+                                    required
+                                    autoComplete="current-password"
+                                    type="password"
+                                />
+
+                                <div>
+                                    <Button
+                                        type="submit"
+                                        className="w-full"
+                                        loading={loggingIn}
+                                    >
+                                        Sign in
+                                    </Button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </AuthLayout>
+            </AuthLayout>
+        </>
     )
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -5,6 +5,8 @@ import { Link, useNavigate } from "react-router-dom";
 import AuthInput from "../components/AuthInput";
 import AuthLayout from "../components/layouts/AuthLayout";
 import Logo from "../components/Logo";
+// @ts-expect-error(no type definitions found )
+import { Helmet } from "react-helmet";
 
 export default function Register() {
     const [registering, setRegistering] = useState(false);
@@ -25,67 +27,72 @@ export default function Register() {
     }
 
     return (
-        <AuthLayout>
-            <div className="mx-auto w-full max-w-sm lg:w-96">
-                <div>
-                    <Link to={'/'}>
-                        <Logo />
-                    </Link>
-                    <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
-                    </h2>
-                    <p className="mt-2 text-sm leading-6 text-gray-500">
-                        Already have an account{' '}
-                        <Link to="/login" className="font-semibold text-blue-600 hover:text-blue-500">
-                            Login
-                        </Link>
-                    </p>
-                </div>
-
-                <div className="mt-10">
+        <>
+            <Helmet>
+                <title>Create account</title>
+            </Helmet>
+            <AuthLayout>
+                <div className="mx-auto w-full max-w-sm lg:w-96">
                     <div>
-                        <form
-                            onSubmit={handleSubmit}
-                            className="space-y-6">
-                            <AuthInput
-                                label="Full Name"
-                                required
-                                type="text"
-                            />
+                        <Link to={'/'}>
+                            <Logo />
+                        </Link>
+                        <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                            Sign in to your account
+                        </h2>
+                        <p className="mt-2 text-sm leading-6 text-gray-500">
+                            Already have an account{' '}
+                            <Link to="/login" className="font-semibold text-blue-600 hover:text-blue-500">
+                                Login
+                            </Link>
+                        </p>
+                    </div>
 
-                            <AuthInput
-                                label="Email Address"
-                                required
-                                autoComplete="email"
-                                type="email"
-                            />
+                    <div className="mt-10">
+                        <div>
+                            <form
+                                onSubmit={handleSubmit}
+                                className="space-y-6">
+                                <AuthInput
+                                    label="Full Name"
+                                    required
+                                    type="text"
+                                />
 
-                            <AuthInput
-                                label="Password"
-                                required
-                                autoComplete="current-password"
-                                type="password"
-                            />
-                            <AuthInput
-                                label="Repeat Password"
-                                required
-                                autoComplete="current-password"
-                                type="password"
-                            />
+                                <AuthInput
+                                    label="Email Address"
+                                    required
+                                    autoComplete="email"
+                                    type="email"
+                                />
 
-                            <div>
-                                <Button
-                                    type="submit"
-                                    className="w-full"
-                                    loading={registering}
-                                >
-                                    Create account
-                                </Button>
-                            </div>
-                        </form>
+                                <AuthInput
+                                    label="Password"
+                                    required
+                                    autoComplete="current-password"
+                                    type="password"
+                                />
+                                <AuthInput
+                                    label="Repeat Password"
+                                    required
+                                    autoComplete="current-password"
+                                    type="password"
+                                />
+
+                                <div>
+                                    <Button
+                                        type="submit"
+                                        className="w-full"
+                                        loading={registering}
+                                    >
+                                        Create account
+                                    </Button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </AuthLayout>
+            </AuthLayout>
+        </>
     )
 }
