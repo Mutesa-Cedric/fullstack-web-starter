b5f9fa1fbf6f80a343469ad93fc941a89d044ac4
Mutesa-Cedric <mutesacedric@gmail.com>
2024-06-10 16:36:42 +0200

    init : navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NotFound from "./pages/404";
+import DashboardLayout from "./components/layouts/DashboardLayout";
+import Overview from "./pages/dashboard/Overview";
+import Products from "./pages/dashboard/Products";
+import Orders from "./pages/dashboard/Orders";
+import Profile from "./pages/dashboard/Profile";
 
 function App() {
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center text-2xl font-semibold text-gray-600">
-      Hello world
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="" element={<Overview />} />
+          <Route path="products" element={<Products />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,9 @@
+import AuthLayout from "../components/layouts/AuthLayout";
+
+export default function Signup() {
+    return (
+        <AuthLayout>
+            Singup
+        </AuthLayout>
+    )
+}
